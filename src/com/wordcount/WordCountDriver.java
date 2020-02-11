609c81a26f0ed3468579210c35c45319cc59009e
FerdinandWang <603334301@qq.com>
2020-02-12 00:23:25 +0800

    wordcount1
    输出单词 1
    每出现一次 写入结果文件一行

---
+++ src/com/wordcount/WordCountDriver.java
@@ -0,0 +1,44 @@
+package com.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class WordCountDriver {
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration configuration = new Configuration();
+		//获取job对象
+		Job job = Job.getInstance();
+		
+		//设置job方法入口的驱动类
+		job.setJarByClass(WordCountDriver.class);
+		
+		//设置Mapper组件类
+		job.setMapperClass(WordCountMapper1.class);
+		
+		//设置Mapper的输出key类型
+		job.setMapOutputKeyClass(Text.class);
+		
+		//设置Mapper的输出value类型
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.setInputPaths(job, 
+				new org.apache.hadoop.fs.Path("hdfs://192.168.1.130:9000/word"));
+		
+		//输出结果路径要求 是一个不存在的目录
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.1.130:9000/word/result"));
+		
+		//提交job
+		job.waitForCompletion(true);
+	}
+
+}
--- src/com/wordcount/WordCountMapper.java
+++ src/com/wordcount/WordCountMapper.java
@@ -1,5 +1,7 @@
 package com.wordcount;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -7,9 +9,27 @@ import org.apache.hadoop.mapreduce.Mapper;
 /**
  * 
  * @author Ferdinand Wang
- * 1.job的MapTask如何处理文件块数据
- *
+ * 1.job的MapTask如何处理文件块数据,是由Mapper组件类来决定，此类里的代码需要开发者来写
+ * 2.开发一个Mapper组件的方式是让一个类继承Mapper
+ * 3.第一个泛型类型对应的是MapTask的输入Key类型
+ * 4.第二个泛型类型对应的是MapTask的输入Value类型
+ * 5.输入key是每行的行首偏移量，所以是LongWritable
+ * 6.输入value是每行的内容，所以是Text
+ * 7.Writable机制是Hadoop的序列化机制
+ * 常用类型LongWritable，Text，NullWritable
+ * 8.map()方法用于将输入key和输入value传给程序员，一行数据调用一次
+ * 9.第三个泛型类型是MapTask的输出key类型
+ * 10.第四个泛型类型是MapTask的输出value类型
+ * 11.第一个和第二个泛型类型写死，第三个和第四个类型不固定
+ * 
  */
 public class WordCountMapper extends Mapper<LongWritable, Text, LongWritable, Text>{
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, LongWritable, Text>.Context context)
+			throws IOException, InterruptedException {
+		
+		//将输入key和输入value做输出
+		context.write(key, value);
+	}
 	
 }
