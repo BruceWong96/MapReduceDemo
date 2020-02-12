7cc36b0f0c6bf86078b01a02f388d91d124ddabd
FerdinandWang <603334301@qq.com>
2020-02-12 21:55:15 +0800

    引入插件+求MR最大值
    1.引入插件开发
    2.用mapreduce求最大值

--- src/com/max/MaxDriver.java
+++ src/com/max/MaxDriver.java
@@ -1,7 +1,6 @@
-package com.wordcount;
+package com.max;
 
 import java.io.IOException;
-
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -11,39 +10,30 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
-public class WordCountDriver {
+public class MaxDriver {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration configuration = new Configuration();
-		//获取job对象
+		
 		Job job = Job.getInstance();
 		
-		//设置job方法入口的驱动类
-		job.setJarByClass(WordCountDriver.class);
-		
-		//设置Mapper组件类
-		job.setMapperClass(WordCountMapper1.class);
-		
-		//设置Mapper的输出key和value类型
+		job.setMapperClass(MaxMapper.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
-		//设置reducer组件类
-		job.setReducerClass(WordCountReducer2.class);
-		
-		//设置reducer的key和value输出类型
+		job.setReducerClass(MaxReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+
 		
 		FileInputFormat.setInputPaths(job, 
-				new org.apache.hadoop.fs.Path("hdfs://192.168.1.130:9000/word"));
+				new org.apache.hadoop.fs.Path("hdfs://192.168.1.130:9000/max"));
 		
 		//输出结果路径要求 是一个不存在的目录
-		FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.1.130:9000/word/result"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.1.130:9000/max/result"));
 		
-		//提交job
 		job.waitForCompletion(true);
+		
+		
 	}
 
 }
